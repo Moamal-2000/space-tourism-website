713dd99f937843545e8737ac28fea45cbb910608
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2023-07-24 16:38:33 +0300

    animation, active explorer button some edit

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,23 +1,26 @@
+import { NavLink } from "react-router-dom";
 import styles from "./Home.module.css";
 
 const Home = () => {
   return (
     <main className={styles.homePage}>
       <div className="container">
-      <section className={styles.text}>
-        <h1>
-          So, you want to travel to
-          <span>Space</span>
-        </h1>
-        <p>
-          Let’s face it; if you want to go to space, you might as well genuinely
-          go to outer space and not hover kind of on the edge of it. Well sit
-          back, and relax because we’ll give you a truly out of this world
-          experience!
-        </p>
-      </section>
+        <section className={styles.text}>
+          <h1>
+            So, you want to travel to
+            <span>Space</span>
+          </h1>
+          <p>
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </section>
 
-      <div className={styles.wordSpace}>Explore</div>
+        <NavLink className={styles.wordSpace} to="/destination">
+          Explore
+        </NavLink>
       </div>
     </main>
   );
