a63d7e82af62f74b64c5708ed017d55f29ba3138
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2023-07-24 06:01:40 +0300

    needed h1 tag in the website ,changed h2 to h1

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,10 +5,10 @@ const Home = () => {
     <main className={styles.homePage}>
       <div className="container">
       <section className={styles.text}>
-        <h2>
+        <h1>
           So, you want to travel to
           <span>Space</span>
-        </h2>
+        </h1>
         <p>
           Let’s face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
