16eba8e8f0ed2ad8ae1ac276875b7657c4bd89ad
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2023-07-25 11:37:43 +0300

    cursor effect in the whole website

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import styles from "./App.module.css";
 import Crew from "./Components/Crew/Crew";
@@ -10,6 +10,15 @@ import { OverlayContext } from "./Hooks/OverlayContext";
 
 function App() {
   const [isOverlayActive, setIsOverlayActive] = useState(false);
+  const cursorEffect = useRef(null)
+
+
+  useEffect(() => {
+    document.addEventListener("mousemove", (e) => {
+      cursorEffect.current.style.background = `radial-gradient(400px at ${e.clientX}px ${e.clientY}px, rgba(29, 78, 100, 0.2), transparent 100%)`;
+    });
+  }, [])
+
 
   return (
     <div className="App">
@@ -22,6 +31,8 @@ function App() {
             setIsOverlayActive(false);
           }}
         ></div>
+
+        <div className={styles.effectScreen} ref={cursorEffect}></div>
 
         <Header />
         <Routes>
