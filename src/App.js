211d49c9c671e9a03d86fe7632949f8df45dbe2b
Moamal-2000 <103026211+Moamal-2000@users.noreply.github.com>
2023-07-26 08:18:06 +0300

    optimization App.js, simple test #4

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import { useEffect, useRef, useState } from "react";
 import { Route, Routes } from "react-router-dom";
-import styles from "./_App.module.scss";
 import Crew from "./Components/Crew/Crew";
 import Destination from "./Components/Destination/Destination";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
 import Technology from "./Components/Technology/Technology";
 import { OverlayContext } from "./Hooks/OverlayContext";
+import styles from "./_App.module.scss";
 
 function App() {
   const [isOverlayActive, setIsOverlayActive] = useState(false);
   const cursorEffect = useRef(null);
 
   useEffect(() => {
-    document.addEventListener("mousemove", (e) => {
+    function drawCursorEffect(e) {
       cursorEffect.current.style.background = `radial-gradient(400px at ${e.clientX}px ${e.clientY}px, rgba(29, 78, 100, 0.2), transparent 100%)`;
-    });
+    }
+
+    document.addEventListener("mousemove", (e) => drawCursorEffect(e));
+
+    return () =>
+      document.removeEventListener("mousemove", () => drawCursorEffect);
   }, []);
 
   return (
@@ -31,6 +36,7 @@ function App() {
         <div className={styles.effectScreen} ref={cursorEffect}></div>
 
         <Header />
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/destination/*" element={<Destination />} />
